8a8aee2bcd3080270ed46838de88d040e8898c62
andro2ss <a.brzeski@hotmail.com>
2022-03-04 04:02:03 -0800

    added validation for all form components

--- src/functions/validate.js
+++ src/functions/validate.js
@@ -3,9 +3,25 @@ export const validate = (values) => {
   const requiredFields = ["name", "preparation_time", "type"];
   requiredFields.forEach((field) => {
     if (!values[field]) {
-      errors[field] = "Required";
+      errors[field] = "Required " + field.replace(/_/g, " ");
     }
   });
+  if (values["type"] === "pizza") {
+    if (!values["no_of_slices"]) {
+      errors["no_of_slices"] = "Required number of slices";
+    }
+    if (!values["diameter"]) {
+      errors["diameter"] = "Required diameter";
+    }
+  } else if (values["type"] === "soup") {
+    if (!values["spiciness_scale"]) {
+      errors["spiciness_scale"] = "Required Spiciness scale";
+    }
+  } else if (values["type"] === "sandwich") {
+    if (!values["slices_of_bread"]) {
+      errors["slices_of_bread"] = "Required slices of bread";
+    }
+  }
 
   return errors;
 };
