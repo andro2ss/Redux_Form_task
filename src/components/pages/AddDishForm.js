2662fdb84efb44d71f8ab9777d92a3a2770908c7
andro2ss <a.brzeski@hotmail.com>
2022-03-04 02:59:10 -0800

    finished layout form, open submit and validation components

--- src/components/pages/AddDishForm.js
+++ src/components/pages/AddDishForm.js
@@ -1,17 +1,24 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
+import { Field, formValueSelector, reduxForm } from "redux-form";
 import { validate } from "../../functions/validate";
 import durationFormatNormalize from "../../functions/durationFormatNormalize";
-import { renderTextField } from "../common/Form/TextField/renderTextField";
+import { renderTextField } from "../common/Form/textField/renderTextField";
 import renderRadioButtonsGroup from "../common/Form/radioButtonsGroup/renderRadioButtonsGroup";
+import { connect } from "react-redux";
+import renderSliderValues from "../common/Form/slider/renderSliderValues";
+import { Button } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 
-const AddDishForm = (props) => {
-  const { handleSubmit, pristine, reset, submitting } = props;
+let AddDishForm = (props) => {
+  const { handleSubmit, pristine, reset, submitting, dishType } = props;
 
   return (
     <form onSubmit={handleSubmit} className="form__container">
       <h2>Add a dish</h2>
+
       <Field name="name" type="text" component={renderTextField} label="Name" />
+
       <Field
         name="preparation_time"
         type="text"
@@ -19,6 +26,7 @@ const AddDishForm = (props) => {
         label="Preparation time"
         normalize={durationFormatNormalize}
       />
+
       <Field
         name="type"
         component={renderRadioButtonsGroup}
@@ -26,17 +34,73 @@ const AddDishForm = (props) => {
         types={["pizza", "soup", "sandwich"]}
       />
 
-      <div>
-        <button type="submit" disabled={submitting}>
-          Submit
-        </button>
-        <button type="button" disabled={pristine || submitting} onClick={reset}>
-          Clear Values
-        </button>
+      {dishType === "pizza" ? (
+        <>
+          <Field
+            name="no_of_slices"
+            type="number"
+            component={renderTextField}
+            label="Number of slices"
+          />
+          <Field
+            name="diameter"
+            type="number"
+            component={renderTextField}
+            label="Diameter"
+          />
+        </>
+      ) : (
+        ""
+      )}
+      {dishType === "soup" ? (
+        <Field
+          name="spiciness_scale"
+          component={renderSliderValues}
+          label={"Spiciness scale"}
+        />
+      ) : (
+        ""
+      )}
+      {dishType === "sandwich" ? (
+        <Field
+          name="slices_of_bread"
+          type="number"
+          component={renderTextField}
+          label="Number of slices of bread"
+        />
+      ) : (
+        ""
+      )}
+      <div className="form__buttons">
+        <Button
+          type="submit"
+          variant="contained"
+          disabled={submitting}
+          className="form__button"
+        >
+          Add <SendIcon className="form__item" />
+        </Button>
+        <Button
+          type="button"
+          variant="outlined"
+          disabled={pristine || submitting}
+          onClick={reset}
+          className="form__button"
+        >
+          Reset <DeleteOutlineIcon className="form__item" />
+        </Button>
       </div>
     </form>
   );
 };
+
+const selector = formValueSelector("addDishForm");
+AddDishForm = connect((state) => {
+  const dishType = selector(state, "type");
+  return {
+    dishType,
+  };
+})(AddDishForm);
 
 export default reduxForm({
   form: "addDishForm",
