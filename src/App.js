80e75183a2b1d48c3973d0e4d319e32f2493746e
andro2ss <a.brzeski@hotmail.com>
2022-03-03 18:20:21 -0800

    created main layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import AddDishForm from "./components/pages/AddDishForm";
+import { Container } from "@mui/material";
+import FixedBottomNavigation from "./components/footer/FixedBottomNavigation";
+
 function App() {
-  return <div className="App">Init</div>;
+  return (
+    <div className="App">
+      <Container maxWidth="sm">
+        <AddDishForm />
+      </Container>
+      <FixedBottomNavigation />
+    </div>
+  );
 }
 
 export default App;
