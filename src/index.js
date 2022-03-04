9956092d4b53d1a47b1da3f889dbdd44b8ef229c
andro2ss <a.brzeski@hotmail.com>
2022-03-03 18:24:24 -0800

    added redux functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { compose, createStore } from "redux";
+import allReducer from "./reducers";
+import { Provider } from "react-redux";
+import "./style/main.scss";
+
+const store = createStore(
+  allReducer,
+  compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
