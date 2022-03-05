a25c64acb4f27dfadc6afa690e53f770cdf61120
andro2ss <a.brzeski@hotmail.com>
2022-03-05 06:50:24 -0800

    added validation for preparation time

--- src/functions/validate.js
+++ src/functions/validate.js
@@ -6,6 +6,13 @@ export const validate = (values) => {
       errors[field] = "Required " + field.replace(/_/g, " ");
     }
   });
+
+  if (!!values["preparation_time"]) {
+    if (values["preparation_time"].length < 8) {
+      errors["preparation_time"] = "Required full time xx:xx:xx";
+    }
+  }
+
   if (values["type"] === "pizza") {
     if (!values["no_of_slices"]) {
       errors["no_of_slices"] = "Required number of slices";
