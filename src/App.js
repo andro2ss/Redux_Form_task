3e789ee843f6667034f46e19d618ba55dbea3908
andro2ss <a.brzeski@hotmail.com>
2022-03-05 06:48:34 -0800

    added form success message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import AddDishForm from "./components/pages/AddDishForm";
+import AddDishForm from "./components/form/AddDishForm";
 import { Container } from "@mui/material";
 import FixedBottomNavigation from "./components/footer/FixedBottomNavigation";
+import handleSubmit from "./functions/handleSubmit";
+import { AlertSuccesSend } from "./components/form/AlertSuccesSend";
 
 function App() {
   return (
     <div className="App">
-      <Container maxWidth="sm">
-        <AddDishForm />
+      <Container maxWidth="sm" id="formContainer">
+        <AddDishForm onSubmit={handleSubmit} />
+        <AlertSuccesSend
+          onClick={() => {
+            window.location.reload();
+          }}
+        />
       </Container>
       <FixedBottomNavigation />
     </div>
